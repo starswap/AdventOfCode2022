2610ef6fcfdd0a6d7dd9f38e3e9533e7a03437ac
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-12-02 11:41:50 +0000

    Solve day 2

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,35 @@
+import java.io.File
+
+const val oppMin = 'A'
+const val youMin = 'X'
+
+fun score(thisTurn: Pair<Int, Int>): Int {
+    val (opp, you) = thisTurn
+    val rockPaperScissors = you + 1
+    val winDrawLose = ((you - opp + 4) % 3)*3
+    return winDrawLose + rockPaperScissors
+}
+
+fun transformRd2(thisTurn: Pair<Int,Int>): Pair<Int, Int> {
+    val (opp, result) = thisTurn
+    return Pair(opp,((opp + result - 1)+3) % 3)
+}
+
+fun part1(thisTurn: List<Pair<Int,Int>>): Int {
+    return thisTurn.map(::score).sum()
+}
+
+fun part2(thisTurn: List<Pair<Int,Int>>): Int {
+    return thisTurn.map(::transformRd2).map(::score).sum()
+}
+
+fun main() {
+    val contents = File("input/Day2.txt").readLines()
+    val turns = contents.map {
+        Pair<Int, Int>(it.split(" ")[0][0].code - oppMin.code,
+                       it.split(" ")[1][0].code - youMin.code)
+    }
+    println("Part 1 answer ${part1(turns)}")
+    println("Part 2 answer ${part2(turns)}")
+
+}
