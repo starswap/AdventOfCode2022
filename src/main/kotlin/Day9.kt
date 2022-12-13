38e8eaea7ac829aec79d19824645466d2b382cbf
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-12-13 22:45:03 +0000

    Solve part 2 of day 9

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -14,40 +14,45 @@ data class Point(var x: Int, var y: Int) {
     fun deltaTo(other: Point) = Vec(other.x-this.x,other.y-this.y)
 }
 
-data class Snake(val head: Point, val tail: Point) {
+data class Snake(val sec: List<Point>) {
     private val delta = mapOf<String, Vec>(
         "D" to Vec(0,-1),
         "U" to Vec(0,1),
         "L" to Vec(-1,0),
         "R" to Vec(1,0)
     )
-    private fun updateTail() {
+    private fun updateTail() {sec.zipWithNext().forEach(::updateOne)}
+
+    private fun updateOne(p: Pair<Point, Point>) {
+        val (head, tail) = p
         val d: Vec = tail.deltaTo(head)
         if (abs(d.dx) > 1 || abs(d.dy) > 1) { // need to catch up
             if (d.dx == 0 || d.dy == 0) { // horizontally or vertically
                 tail.transform(d.divide(abs(d.dx + d.dy)))
-            }
-            else {
+            } else {
                 tail.transform(d.signs())
             }
         }
     }
+
     fun move(cmd: String) {
-        head.transform(delta[cmd]!!)
+        sec[0].transform(delta[cmd]!!)
         updateTail()
     }
 }
 
 object Day9 {
-    private val snake: Snake = Snake(Point(0,0), Point(0,0))
-    fun part1(lines: List<String>): Int {
+    fun part1(lines: List<String>): Int = process(lines, Snake(List(2) { Point(0, 0) }))
+    fun part2(lines: List<String>): Int = process(lines, Snake(List(10) { Point(0, 0) }))
+
+    fun process(lines: List<String>, snake: Snake): Int {
         val visited: MutableSet<Point> = mutableSetOf<Point>()
-        visited.add(snake.tail.copy())
+        visited.add(snake.sec.last().copy())
         for (line in lines) {
             val (command, dist) = line.split(" ")
             repeat(dist.toInt()) {
                 snake.move(command)
-                visited.add(snake.tail.copy())
+                visited.add(snake.sec.last().copy())
             }
         }
         return visited.size
@@ -57,4 +62,5 @@ object Day9 {
 fun main() {
     val lines: List<String> = File("input/Day9.txt").readLines()
     println("Part 1 ans: ${Day9.part1(lines)}")
+    println("Part 2 ans: ${Day9.part2(lines)}")
 }
